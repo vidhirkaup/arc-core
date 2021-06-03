eb15d94a711edabe52fd900514600cdf9585a7f6
vidhirkaup <vidhir.kaup@gmail.com>
2021-06-02 22:29:12 -0400

    added support for async events

---
+++ src/main/java/com/vlabs/arc/core/events/SpringEventsConfig.java
@@ -0,0 +1,18 @@
+package com.vlabs.arc.core.events;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ApplicationEventMulticaster;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class SpringEventsConfig {
+
+    @Bean(name = "applicationEventMulticaster")
+    public ApplicationEventMulticaster simpleApplicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return eventMulticaster;
+    }
+}
